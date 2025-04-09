a77c78edd934534543553055a7674b091a48caca
Solsolol <109537308+Solsolol@users.noreply.github.com>
2025-04-09 10:25:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,34 @@
 const express = require('express');
 const app = express();
 
+// Enable JSON parsing
+app.use(express.json());
+
+// Add config.json route
+app.get('/config.json', (req, res) => {
+  res.json({
+    workflowApiVersion: '1.1',
+    metaData: {
+      icon: 'images/icon.png',
+      category: 'message'
+    },
+    type: 'REST',
+    lang: {
+      'en-US': {
+        name: 'SMS Custom Activity',
+        description: 'A custom SMS activity'
+      }
+    },
+    arguments: {
+      execute: {
+        inArguments: [],
+        outArguments: [],
+        url: 'https://demosmscustom-3a3ca3cd2daa.herokuapp.com/execute'
+      }
+    }
+  });
+});
+
 // Basic route for testing
 app.get('/', (req, res) => {
   res.send('Server is running!');
